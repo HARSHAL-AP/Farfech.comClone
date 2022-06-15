3b0a2324d9170f94165bd04a1edc2a9eb6d9e24d
HARSHAL-AP <apsundeharshal129@gmail.com>
2022-06-15 11:34:46 +0530

    home page

---
+++ script/index.js
@@ -0,0 +1,28 @@
+var modal=document.getElementById("myModal");
+
+var btn = document.getElementById("searchbutn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
